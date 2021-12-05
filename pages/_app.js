cbbe1a2cd18a507b4a37094d0b1d8252625b7539
Guilherme Toledo <totoledao@hotmail.com>
2021-12-05 11:38:11 -0300

    Created Context API

--- pages/_app.js
+++ pages/_app.js
@@ -7,11 +7,17 @@ import { CacheProvider } from '@emotion/react';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
 
+import AppContext from '../src/AppContext';
+
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
 
 export default function MyApp(props) {
   const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  //Global States
+  const [selectedMenu, setSelectedMenu] = React.useState();
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = React.useState(false);
 
   return (
     <CacheProvider value={emotionCache}>
@@ -19,11 +25,22 @@ export default function MyApp(props) {
         <title>My page</title>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </Head>
-      <ThemeProvider theme={theme}>
-        {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
-        <CssBaseline />
-        <Component {...pageProps} />
-      </ThemeProvider>
+      <AppContext.Provider 
+        value={
+          {
+            selectedMenu,
+            setSelectedMenu,
+            isMobileMenuOpen,
+            setIsMobileMenuOpen
+          }
+        }
+      >
+        <ThemeProvider theme={theme}>
+          {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
+          <CssBaseline />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </AppContext.Provider>
     </CacheProvider>
   );
 }
